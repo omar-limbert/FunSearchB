1a56510687cb10a27c41fc09b0e02fbdf78c8ea4
Omar Limbert Huanca Sanchez <omar.limbert@gmail.com>
2018-03-28 16:28:42 -0400

    main fixed xD
    sorry main fixed

--- src/com/fundation/search/Main.java
+++ src/com/fundation/search/Main.java
@@ -9,20 +9,5 @@ import javax.swing.*;
  */
 public class Main {
 
-    /**
-     * Constructor for CentralPanel.
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
-
-        final int width = 600;
-        final int height = 400;
-
-        MainSearchWindows windows = new MainSearchWindows();
-        windows.initWindows("Search Application");
-        windows.setSize(width, height);
-        windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        windows.setVisible(true);
     }
 }
