5e8eaab7c6a0e52462ea78ed8d1951a174524edc
pipo411 <Ariel.Gonzales@fundacion-jala.org>
2018-03-29 17:19:27 -0400

    Main

--- src/com/fundation/search/Main.java
+++ src/com/fundation/search/Main.java
@@ -1,15 +1,17 @@
 package com.fundation.search;
 
 
+import com.fundation.search.controller.ControlCriteria;
+import com.fundation.search.view.MainSearchWindows;
+
 /**
  * Main class .
  */
 public class Main {
 
     public static void main(String[] args) {
-
-
-        
+        MainSearchWindows searchWindow = new MainSearchWindows();
+        ControlCriteria controlCriteria = new ControlCriteria(searchWindow);
 
     }
 }
