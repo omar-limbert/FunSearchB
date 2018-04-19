afc9d4a372e635a7051a9d03c926dfaaedd8bfd6
Omar Limbert Huanca Sanchez <omar@programadorez.com>
2018-04-19 19:17:20 -0400

    COMMAND-LINE-TABLE
    COMMAND-LINE-TABLE for View

---
+++ src/com/fundation/search/common/table/Align.java
@@ -0,0 +1,26 @@
+/*
+ * @(#)SearchCriteria.java
+ *
+ * Copyright (c) 2018 Jala Foundation.
+ * Address
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Sun.
+ */
+package com.fundation.search.common.table;
+
+/**
+ * This class is make SearchCriteria with Builder Pattern.
+ *
+ * @author Omar Limbert Huanca sanchez - AT-[06]
+ * @version 1.0.
+ */
+public enum Align {
+    LEFT,
+    CENTER,
+    RIGHT;
+}
