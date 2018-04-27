8387781bd88a1c929747e05af3ce3869cffa0a32
Christian Galarza <Christian.Galarza@fundacion-jala.org>
2018-04-27 05:19:00 -0400

    UnitTest Fixed

--- test/com/fundation/search/controller/ValidatorTest.java
+++ test/com/fundation/search/controller/ValidatorTest.java
@@ -47,11 +47,18 @@ public class ValidatorTest {
      */
     @Test
     public void testValidPaths() {
-        assertTrue(validator.isValidPath("C:\\Users\\Ariel Gonzales\\Desktop\\Fundacion Jala\\Ingles"));
-        assertTrue(validator.isValidPath("D:\\DESCARGAS\\jre1.8.0_162\\bin\\dtplugin"));
-        assertTrue(validator.isValidPath("C:\\MSI\\GamingAPP\\Hotkey\\Script"));
-        assertTrue(validator.isValidPath("C:\\Users\\Ariel Gonzales\\Downloads\\The witcher\\The_Witcher_wallpapers\\1600 x 1200"));
-        assertTrue(validator.isValidPath("C:\\Users\\Ariel Gonzales\\Downloads\\CINEBENCHR15.038\\CINEBENCH R15.038_RC184115\\modules\\mograph\\res\\strings_us\\description"));
+
+        assertTrue(validator.isValidPath("C:\\Program Files"));
+        assertTrue(validator.isValidPath("C:\\Program Files\\Java"));
+        assertTrue(validator.isValidPath("C:\\Program Files\\JetBrains"));
+        assertTrue(validator.isValidPath("C:\\Windows"));
+        assertTrue(validator.isValidPath("C:\\Users"));
+        assertTrue(validator.isValidPath("C:\\Users\\Public"));
+        assertTrue(validator.isValidPath("C:\\Users\\Public\\Documents"));
+        assertTrue(validator.isValidPath("C:\\Program Files (x86)\\Internet Explorer"));
+        assertTrue(validator.isValidPath("C:\\Program Files (x86)\\Windows Media Player"));
+        assertTrue(validator.isValidPath("C:\\Program Files (x86)\\WindowsPowerShell"));
+
     }
 
     /**
@@ -60,9 +67,14 @@ public class ValidatorTest {
      */
     @Test
     public void testInvalidPaths() {
-        assertFalse(validator.isValidPath("C:\\Users\\Ariel Gonzales\\Downloads\\\\Motospeed V20"));
-        assertFalse(validator.isValidPath("C:\\Users\\Ariel Gonzales\\Downloads\\The witcher\\The_Witcher_wallpapers\\1600 x ****"));
-        assertFalse(validator.isValidPath("D:\\D:\\DESCARGAS\\\\jre1.8.0_162\\\\bin\\\\dtplugin\""));
+        assertFalse(validator.isValidPath("C:\\Program Files (x86)\\WindowsPowerShe"));
+        assertFalse(validator.isValidPath("C:\\Downoad"));
+        assertFalse(validator.isValidPath("C\\Program Files (x)\\WinRAR"));
+        assertFalse(validator.isValidPath("C:\\ou"));
+        assertFalse(validator.isValidPath("D:\\Download"));
+        assertFalse(validator.isValidPath("C:\\Program Files (x86)\\Javac"));
+        assertFalse(validator.isValidPath("C:\\Program Files (x86)\\Doc"));
+        assertFalse(validator.isValidPath("G:\\Users"));
 
     }
 
@@ -75,6 +87,8 @@ public class ValidatorTest {
     @Test
     public void testIsValidPath() {
         assertTrue(validator.isValidPath("C:\\Program Files (x86)"));
+        assertTrue(validator.isValidPath("C:\\Windows"));
+        assertTrue(validator.isValidPath("C:\\Users"));
     }
 
 
@@ -104,6 +118,11 @@ public class ValidatorTest {
         assertFalse(validator.isValidFile("RG-D<>_MI-002-05"));
         assertFalse(validator.isValidFile("The_Witcher|_Toruviel"));
         assertFalse(validator.isValidFile("MotoSpeed \"\"Gaming MouseV20"));
+        assertFalse(validator.isValidFile("https:\\www.google.com.bo/?gws_rd=cr,ssl&ei=zUXFVaCtHYW8wASKzbOgBw"));
+        assertFalse(validator.isValidFile("lelouch@Jose:~$"));
+        assertFalse(validator.isValidFile("/watch?v:=t:hsaWzy9PdM"));
+
+
     }
 
     /**
@@ -114,7 +133,13 @@ public class ValidatorTest {
         assertTrue(validator.isValidFileExtension(".doc"));
         assertTrue(validator.isValidFileExtension(".exe"));
         assertTrue(validator.isValidFileExtension(".pack"));
-        assertTrue(validator.isValidFileExtension(".dll"));
+        assertTrue(validator.isValidFileExtension(".txt"));
+        assertTrue(validator.isValidFileExtension(".rar"));
+        assertTrue(validator.isValidFileExtension(".zip"));
+        assertTrue(validator.isValidFileExtension(".jpg"));
+        assertTrue(validator.isValidFileExtension(".png"));
+        assertTrue(validator.isValidFileExtension(".pdf"));
+        assertTrue(validator.isValidFileExtension(".gif"));
     }
 
     /**
@@ -127,6 +152,15 @@ public class ValidatorTest {
         assertFalse(validator.isValidFileExtension(".treepack"));
         assertFalse(validator.isValidFileExtension("..dll"));
         assertFalse(validator.isValidFileExtension(".gg"));
+        assertFalse(validator.isValidFileExtension("."));
+        assertFalse(validator.isValidFileExtension(".png."));
+        assertFalse(validator.isValidFileExtension(".jpg.."));
+        assertFalse(validator.isValidFileExtension("?gif"));
+        assertFalse(validator.isValidFileExtension("!ini"));
+        assertFalse(validator.isValidFileExtension("..txt?"));
+        assertFalse(validator.isValidFileExtension("?!avi.."));
+        assertFalse(validator.isValidFileExtension("doc...?"));
+        assertFalse(validator.isValidFileExtension(""));
     }
 
     /**
@@ -155,7 +189,78 @@ public class ValidatorTest {
         assertFalse(validator.isValidSize("158.3.0"));
         assertFalse(validator.isValidSize("25.87.4"));
         assertFalse(validator.isValidSize("25.87 4"));
+        assertFalse(validator.isValidSize(""));
+        assertFalse(validator.isValidSize("hello"));
+        assertFalse(validator.isValidSize("@"));
+        assertFalse(validator.isValidSize("-*"));
+        assertFalse(validator.isValidSize("?45"));
+        assertFalse(validator.isValidSize("☻"));
+        assertFalse(validator.isValidSize(",."));
+        assertFalse(validator.isValidSize("45!6"));
+        assertFalse(validator.isValidSize("´<>"));
+        assertFalse(validator.isValidSize("\\"));
 
     }
+    @Test
+    public void testValidatorDate() {
+        assertTrue(validator.isValidDate("21/03/1995"));
+        assertTrue(validator.isValidDate("12/2/1994"));
+        assertTrue(validator.isValidDate("25/12/95"));
+        assertTrue(validator.isValidDate("1/1/2000"));
+        assertTrue(validator.isValidDate("16/Mar/99"));
+        assertTrue(validator.isValidDate("18/8/10"));
+
+
+    }
+
+
+    @Test
+    public void testInvalidatorDate() {
+        assertFalse(validator.isValidDate(""));
+        assertFalse(validator.isValidDate("☻"));
+        assertFalse(validator.isValidDate("656holas65465654654"));
+        assertFalse(validator.isValidDate(".jpg.."));
+        assertFalse(validator.isValidDate("https:\\www.google.com.bo/?gws_rd=cr,ssl&ei=zUXFVaCtHY"));
+        assertFalse(validator.isValidDate("656holas65465654654"));
+        assertFalse(validator.isValidDate("1/1/2"));
+        assertFalse(validator.isValidDate("16/Mar/22562"));
+        assertFalse(validator.isValidDate("32/Mar/1962"));
+        assertFalse(validator.isValidDate("13/13/13"));
+        assertFalse(validator.isValidDate("16/Abr/2012"));
+        assertFalse(validator.isValidDate("15/Jan/012"));
+        assertFalse(validator.isValidDate("1/ /20"));
+        assertFalse(validator.isValidDate(" /1/2018"));
+        assertFalse(validator.isValidDate("31/1/ "));
+        assertFalse(validator.isValidDate(" / /2018"));
+        assertFalse(validator.isValidDate(" /1/ "));
+        assertFalse(validator.isValidDate(" / / "));
+        assertFalse(validator.isValidDate("31/ / "));
+        assertFalse(validator.isValidDate("Twenty/1/2026"));
+        assertFalse(validator.isValidDate("21/3/ten"));
+        assertFalse(validator.isValidDate("2/10/199"));
+        assertFalse(validator.isValidDate("15th/1/1994"));
+        assertFalse(validator.isValidDate("1/12/7"));
+    }
+
+    @Test
+    public void testValidatorIsTrueFalse() {
+        assertTrue(validator.isTrueFalse("true"));
+        assertTrue(validator.isTrueFalse("false"));
+        assertTrue(validator.isTrueFalse("True"));
+        assertTrue(validator.isTrueFalse("False"));
+
+    }
+
+    @Test
+    public void testInvalidatorIsTrueFalse() {
+        assertFalse(validator.isTrueFalse("troe"));
+        assertFalse(validator.isTrueFalse("fail"));
+        assertFalse(validator.isTrueFalse(""));
+        assertFalse(validator.isTrueFalse(".45"));
+        assertFalse(validator.isTrueFalse("hello"));
+        assertFalse(validator.isTrueFalse("\\"));
+
+    }
+
 
 }
